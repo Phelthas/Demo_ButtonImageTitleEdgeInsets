b320ad33566e08d799171dbcbc1d36ed0a552e1d
Phelthas <luxiaoming@luxiaomingdeiMac.local>
2016-04-01 22:05:01 +0800

    add contentInset

--- Demo_ButtonImageTitleEdgeInsets/LXMButtonImagePosition/UIButton+LXMImagePosition.m
+++ Demo_ButtonImageTitleEdgeInsets/LXMButtonImagePosition/UIButton+LXMImagePosition.m
@@ -11,7 +11,11 @@
 @implementation UIButton (LXMImagePosition)
 
 - (void)setImagePosition:(LXMImagePosition)postion spacing:(CGFloat)spacing {
-    CGFloat imageWith = self.imageView.image.size.width;
+    [self setTitle:self.currentTitle forState:UIControlStateNormal];
+    [self setImage:self.currentImage forState:UIControlStateNormal];
+
+    
+    CGFloat imageWidth = self.imageView.image.size.width;
     CGFloat imageHeight = self.imageView.image.size.height;
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
@@ -19,30 +23,39 @@
     CGFloat labelHeight = [self.titleLabel.text sizeWithFont:self.titleLabel.font].height;
 #pragma clang diagnostic pop
     
-    CGFloat imageOffsetX = (imageWith + labelWidth) / 2 - imageWith / 2;//image中心移动的x距离
+    CGFloat imageOffsetX = (imageWidth + labelWidth) / 2 - imageWidth / 2;//image中心移动的x距离
     CGFloat imageOffsetY = imageHeight / 2 + spacing / 2;//image中心移动的y距离
-    CGFloat labelOffsetX = (imageWith + labelWidth / 2) - (imageWith + labelWidth) / 2;//label中心移动的x距离
+    CGFloat labelOffsetX = (imageWidth + labelWidth / 2) - (imageWidth + labelWidth) / 2;//label中心移动的x距离
     CGFloat labelOffsetY = labelHeight / 2 + spacing / 2;//label中心移动的y距离
+    
+    CGFloat tempWidth = MAX(labelWidth, imageWidth);
+    CGFloat changedWidth = labelWidth + imageWidth - tempWidth;
+    CGFloat tempHeight = MAX(labelHeight, imageHeight);
+    CGFloat changedHeight = labelHeight + imageHeight + spacing - tempHeight;
     
     switch (postion) {
         case LXMImagePositionLeft:
             self.imageEdgeInsets = UIEdgeInsetsMake(0, -spacing/2, 0, spacing/2);
             self.titleEdgeInsets = UIEdgeInsetsMake(0, spacing/2, 0, -spacing/2);
+            self.contentEdgeInsets = UIEdgeInsetsMake(0, spacing/2, 0, spacing/2);
             break;
             
         case LXMImagePositionRight:
             self.imageEdgeInsets = UIEdgeInsetsMake(0, labelWidth + spacing/2, 0, -(labelWidth + spacing/2));
-            self.titleEdgeInsets = UIEdgeInsetsMake(0, -(imageHeight + spacing/2), 0, imageHeight + spacing/2);
+            self.titleEdgeInsets = UIEdgeInsetsMake(0, -(imageWidth + spacing/2), 0, imageWidth + spacing/2);
+            self.contentEdgeInsets = UIEdgeInsetsMake(0, spacing/2, 0, spacing/2);
             break;
             
         case LXMImagePositionTop:
             self.imageEdgeInsets = UIEdgeInsetsMake(-imageOffsetY, imageOffsetX, imageOffsetY, -imageOffsetX);
             self.titleEdgeInsets = UIEdgeInsetsMake(labelOffsetY, -labelOffsetX, -labelOffsetY, labelOffsetX);
+            self.contentEdgeInsets = UIEdgeInsetsMake(imageOffsetY, -changedWidth/2, changedHeight-imageOffsetY, -changedWidth/2);
             break;
             
         case LXMImagePositionBottom:
             self.imageEdgeInsets = UIEdgeInsetsMake(imageOffsetY, imageOffsetX, -imageOffsetY, -imageOffsetX);
             self.titleEdgeInsets = UIEdgeInsetsMake(-labelOffsetY, -labelOffsetX, labelOffsetY, labelOffsetX);
+            self.contentEdgeInsets = UIEdgeInsetsMake(changedHeight-imageOffsetY, -changedWidth/2, imageOffsetY, -changedWidth/2);
             break;
             
         default:
@@ -50,5 +63,6 @@
     }
     
 }
+
 
 @end
--- Demo_ButtonImageTitleEdgeInsets/TEST_ButtonViewController.m
+++ Demo_ButtonImageTitleEdgeInsets/TEST_ButtonViewController.m
@@ -12,16 +12,24 @@
 @interface TEST_ButtonViewController ()
 
 @property (weak, nonatomic) IBOutlet UIButton *originalButton;
-
 @property (weak, nonatomic) IBOutlet UIButton *oneButton;
 @property (weak, nonatomic) IBOutlet UIButton *twoButton;
 @property (weak, nonatomic) IBOutlet UIButton *threeButton;
+@property (weak, nonatomic) IBOutlet UIButton *fourButton;
 
 
 @property (weak, nonatomic) IBOutlet UIButton *originalButton_line;
 @property (weak, nonatomic) IBOutlet UIButton *oneButton_line;
 @property (weak, nonatomic) IBOutlet UIButton *twoButton_line;
 @property (weak, nonatomic) IBOutlet UIButton *threeButton_line;
+@property (weak, nonatomic) IBOutlet UIButton *fourButton_line;
+
+@property (weak, nonatomic) IBOutlet UIButton *originalButton_line_1;
+@property (weak, nonatomic) IBOutlet UIButton *oneButton_line_1;
+@property (weak, nonatomic) IBOutlet UIButton *twoButton_line_1;
+@property (weak, nonatomic) IBOutlet UIButton *threeButton_line_1;
+@property (weak, nonatomic) IBOutlet UIButton *fourButton_line_1;
+
 
 
 @end
@@ -33,34 +41,58 @@
     [self setupDefault];
     
     //图片是60*60的2x的图
-    CGFloat imageWith = 30;
+    CGFloat imageWidth = 30;
     CGFloat imageHeight = 30;
     CGFloat labelWidth = [self.originalButton.titleLabel.text sizeWithFont:self.originalButton.titleLabel.font].width;
     CGFloat labelHeight = [self.originalButton.titleLabel.text sizeWithFont:self.originalButton.titleLabel.font].height;
     
-    //image在后，文字在前
-    self.oneButton.imageEdgeInsets = UIEdgeInsetsMake(0, labelWidth, 0, -labelWidth);
-    self.oneButton.titleEdgeInsets = UIEdgeInsetsMake(0, -imageWith, 0, imageWith);
+    CGFloat spacing = 10;
     
-    [self.oneButton_line setImagePosition:LXMImagePositionRight spacing:10];
+    //image在左，文字在右，default
+    self.oneButton.imageEdgeInsets = UIEdgeInsetsMake(0, -spacing/2, 0, spacing/2);
+    self.oneButton.titleEdgeInsets = UIEdgeInsetsMake(0, spacing/2, 0, -spacing/2);
+    
+    self.oneButton_line.imageEdgeInsets = UIEdgeInsetsMake(0, -spacing/2, 0, spacing/2);
+    self.oneButton_line.titleEdgeInsets = UIEdgeInsetsMake(0, spacing/2, 0, -spacing/2);
+    
+    [self.oneButton_line_1 setImagePosition:LXMImagePositionLeft spacing:spacing];
+    
+    
+    //image在右，文字在左
+    self.twoButton.imageEdgeInsets = UIEdgeInsetsMake(0, labelWidth + spacing/2, 0, -(labelWidth + spacing/2));
+    self.twoButton.titleEdgeInsets = UIEdgeInsetsMake(0, -(imageWidth + spacing/2), 0, imageWidth + spacing/2);
+    
+    self.twoButton_line.imageEdgeInsets = UIEdgeInsetsMake(0, labelWidth + spacing/2, 0, -(labelWidth + spacing/2));
+    self.twoButton_line.titleEdgeInsets = UIEdgeInsetsMake(0, -(imageWidth + spacing/2), 0, imageWidth + spacing/2);
+    
+    [self.twoButton_line_1 setImagePosition:LXMImagePositionRight spacing:spacing];
     
     //image在上，文字在下
-    CGFloat imageOffsetX = (imageWith + labelWidth) / 2 - imageWith / 2;//image中心移动的x距离
-    CGFloat imageOffsetY = imageHeight / 2;//image中心移动的y距离
-    CGFloat labelOffsetX = (imageWith + labelWidth / 2) - (imageWith + labelWidth) / 2;//label中心移动的x距离
-    CGFloat labelOffsetY = labelHeight / 2;//label中心移动的y距离
+    CGFloat imageOffsetX = (imageWidth + labelWidth) / 2 - imageWidth / 2;//image中心移动的x距离
+    CGFloat imageOffsetY = imageHeight / 2 + spacing / 2;//image中心移动的y距离
+    CGFloat labelOffsetX = (imageWidth + labelWidth / 2) - (imageWidth + labelWidth) / 2;//label中心移动的x距离
+    CGFloat labelOffsetY = labelHeight / 2 + spacing / 2;//label中心移动的y距离
     
-    self.twoButton.imageEdgeInsets = UIEdgeInsetsMake(-imageOffsetY, imageOffsetX, imageOffsetY, -imageOffsetX);
-    self.twoButton.titleEdgeInsets = UIEdgeInsetsMake(labelOffsetY, -labelOffsetX, -labelOffsetY, labelOffsetX);
+    self.threeButton.imageEdgeInsets = UIEdgeInsetsMake(-imageOffsetY, imageOffsetX, imageOffsetY, -imageOffsetX);
+    self.threeButton.titleEdgeInsets = UIEdgeInsetsMake(labelOffsetY, -labelOffsetX, -labelOffsetY, labelOffsetX);
     
-    [self.twoButton_line setImagePosition:LXMImagePositionTop spacing:10];
+    self.threeButton_line.imageEdgeInsets = UIEdgeInsetsMake(-imageOffsetY, imageOffsetX, imageOffsetY, -imageOffsetX);
+    self.threeButton_line.titleEdgeInsets = UIEdgeInsetsMake(labelOffsetY, -labelOffsetX, -labelOffsetY, labelOffsetX);
+    
+    [self.threeButton_line_1 setImagePosition:LXMImagePositionTop spacing:spacing];
     
     
-    //image，label中间间隔10
-    self.threeButton.imageEdgeInsets = UIEdgeInsetsMake(0, 0, 0, 0);
-    self.threeButton.titleEdgeInsets = UIEdgeInsetsMake(0, 10, 0, 0);//注意看显示，label因为大小不够文字显示不全了，如果button大小更大的话这样写也没有问题
+    //image在下，文字在上
+    self.fourButton.imageEdgeInsets = UIEdgeInsetsMake(imageOffsetY, imageOffsetX, -imageOffsetY, -imageOffsetX);
+    self.fourButton.titleEdgeInsets = UIEdgeInsetsMake(-labelOffsetY, -labelOffsetX, labelOffsetY, labelOffsetX);
     
-    [self.threeButton_line setImagePosition:LXMImagePositionLeft spacing:10];//这样文字图片都能正常显示，但是button大小没有变化
+    self.fourButton_line.imageEdgeInsets = UIEdgeInsetsMake(imageOffsetY, imageOffsetX, -imageOffsetY, -imageOffsetX);
+    self.fourButton_line.titleEdgeInsets = UIEdgeInsetsMake(-labelOffsetY, -labelOffsetX, labelOffsetY, labelOffsetX);
+    
+    [self.fourButton_line_1 setImagePosition:LXMImagePositionBottom spacing:spacing];
+    
+    
+    [self.originalButton setTitle:@"asdfasdfasdf" forState:UIControlStateNormal];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -80,6 +112,13 @@
     [self setupButton:self.oneButton_line];
     [self setupButton:self.twoButton_line];
     [self setupButton:self.threeButton_line];
+    [self setupButton:self.fourButton_line];
+    
+    [self setupButton:self.originalButton_line_1];
+    [self setupButton:self.oneButton_line_1];
+    [self setupButton:self.twoButton_line_1];
+    [self setupButton:self.threeButton_line_1];
+    [self setupButton:self.fourButton_line_1];
 }
 
 - (void)setupButton:(UIButton *)button {
